13bf94d9b41bb517f788c452da9ea896e4798244
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-23 11:29:47 -0500

    hu3 se crea JS

---
+++ controllers/controladorFormulario.js
@@ -0,0 +1,10 @@
+//scope de las variables de JS
+
+//almacenando una etiqueta HTML en una variable en JS
+//hago esto para poder controlar esa etiqueta desde la logica de JS
+
+let productNamebox=document.getElementById("name")
+let productLastnamebox=document.getElementById("lastname")
+let productEmailbox=document.getElementById("email")
+let productUserbox=document.getElementById("user")
+let productformButtonbox=document.getElementById("formButton")
