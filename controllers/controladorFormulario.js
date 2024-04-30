f7df04d339a4e4888ee39a3d11233859ed9b9270
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-30 11:24:35 -0500

    se hicieron cambios en el JS

--- controllers/controladorFormulario.js
+++ controllers/controladorFormulario.js
@@ -8,3 +8,18 @@ let productLastnamebox=document.getElementById("lastname")
 let productEmailbox=document.getElementById("email")
 let productUserbox=document.getElementById("user")
 let productformButtonbox=document.getElementById("formButton")
+
+//COMO DETECTO UN EVENTO EN JS?
+//1.Una vez tenga una variable que 
+//almacena la etiqueta sobre la cual querio escuchar el evento 
+
+//2.La funcion o metodo propio del JS (perro guardian)
+
+productformButtonbox.addEventListener("click",function(evento){
+    
+    evento.preventDefault()
+    //logica que deseo activar si el eventose detecto con exito 
+    console.log("acaban de darle clic al formulairo")
+
+    //como capturar el valor de un input de un formulario?
+})
