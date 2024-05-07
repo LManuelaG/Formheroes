5297cc2c9de85ed5b0a9439c7adbc97c202cc410
manuela montoya <you@example.com>
2024-05-07 11:35:39 -0500

    cambios

--- controllers/controladorFormulario.js
+++ controllers/controladorFormulario.js
@@ -4,9 +4,11 @@
 //hago esto para poder controlar esa etiqueta desde la logica de JS
 
 let productNamebox=document.getElementById("name")
-let productLastnamebox=document.getElementById("lastname")
-let productEmailbox=document.getElementById("email")
-let productUserbox=document.getElementById("user")
+let productPricebox=document.getElementById("price")
+let productDescriptionbox=document.getElementById("description")
+let productPhotobox=document.getElementById("photo")
+let productReviewbox=document.getElementById("review")
+let productBrandbox=document.getElementById("brand")
 let productformButtonbox=document.getElementById("formButton")
 
 //COMO DETECTO UN EVENTO EN JS?
@@ -16,10 +18,20 @@ let productformButtonbox=document.getElementById("formButton")
 //2.La funcion o metodo propio del JS (perro guardian)
 
 productformButtonbox.addEventListener("click",function(evento){
-    
+
     evento.preventDefault()
     //logica que deseo activar si el eventose detecto con exito 
     console.log("acaban de darle clic al formulairo")
 
     //como capturar el valor de un input de un formulario?
+    let nombreproducto=productNamebox.value
+    let precioproducto=productPricebox.value
+    let descripcionproducto=productDescriptionbox.value
+    let fotoproducto=productPhotobox.value
+    let reseñaproducto=productReviewbox.value
+    let marcaproducto=productBrandbox.value
+
+    //mostrando los valores capturados 
+
+    console.log(nombreproducto,precioproducto,descripcionproducto,fotoproducto,marcaproducto,reseñaproducto)
 })
